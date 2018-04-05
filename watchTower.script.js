0325733400ae3595d9ed2285edea3351f23acd0d
Sapsap11 <38094709+Sapsap11@users.noreply.github.com>
2018-04-05 13:01:08 +0200

    from a small server to a big one
    watchtower now work using a small server to control a bigger server

--- watchTower.script.js
+++ watchTower.script.js
@@ -1,5 +1,5 @@
-servers=["foodnstuff","iron-gym","sigma-cosmetics","nectar-net","harakiri-sushi","phantasy","silver-helix"];
-host=getHostname();
+servers=[server list];
+host="bigger server";
 i=0;
 serverRam=(getServerRam(host)[0]-getServerRam(host)[1])/servers.length;
 while(true)
@@ -11,16 +11,16 @@ while(true)
     {
         if (getServerSecurityLevel(servers[i]) > getServerMinSecurityLevel(servers[i])*5/2)
         {
-            run("weaker.script", Math.floor(serverRam/getScriptRam("weaker.script", host)), servers[i]);
+            exec("weaker.script", host, Math.floor(serverRam/getScriptRam("weaker.script", host)), servers[i]);
         }
         else if (getServerMoneyAvailable(servers[i])<getServerMaxMoney(servers[i])*0.7)
         {
-            run("grower.script", Math.floor(serverRam/getScriptRam("grower.script", host)), servers[i]);
+            exec("grower.script", host, Math.floor(serverRam/getScriptRam("grower.script", host)), servers[i]);
         
         }
         else
         {
-            run("hacker.script", Math.floor(serverRam/getScriptRam("hacker.script", host)), servers[i]);
+            exec("hacker.script", host, Math.floor(serverRam/getScriptRam("hacker.script", host)), servers[i]);
         
         }
     }
