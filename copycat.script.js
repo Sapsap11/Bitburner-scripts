70661a79a004ee42c8f4d382300542f625b62cd0
Sapsap11 <38094709+Sapsap11@users.noreply.github.com>
2018-04-07 13:45:18 +0200

    redo
    better ways to run stuff was found, copycat now spreads the hacking file, attack servers that aren't open, and gets the names using port 1

--- copycat.script.js
+++ copycat.script.js
@@ -1,40 +1,28 @@
 //sets up the needed variables
 host=getHostname();
 servers=scan(host);
+//servers you don't want to hack
 forbiddenServers=["home","darkweb"];
-i=0;
-
-while (i<servers.length)
+//amount of attacking exes you have other than NUKE
+attackPossible=4;
+for (i=0;i<servers.length;i++)
 {
-    //this should terminate the script if needed
-    if (isRunning('terminate.script', 'home'))
-    {
-        exit();
-    }
-    
     //this checks that we can actually hack the server, and prevents it from trying to hit a server we already have
-    unhackable=getHackingLevel()<getServerRequiredHackingLevel(servers[i])
-    if (!(unhackable || isRunning("copycat.script",servers[i]) || servers[i] in forbiddenServers))
+    if (!(isRunning("copycat.script",servers[i])||forbiddenServers.includes(servers[i])))
     {
-    
-        //sets up the files for the worm to keep going
-        scp("genhack.script", servers[i]);
-        scp("attack.script", servers[i]);
-        scp("copycat.script",servers[i]);
-    
-        //gives us root if we don't already have it
-        if (!hasRootAccess(servers[i]) && getServerNumPortsRequired(servers[i])<3)
-        {
-        run("attack.script",1, servers[i]);
-        }
-    
-        //starts copycat on the next node and waits a bit until it starts genhack with as many threads as it can
-        exec("copycat.script",servers[i]);
-    
+    //sets up the files for the worm to keep going
+    killall(servers[i]);
+    scp("copycat.script","home",servers[i]);
+    scp("getName.script","home",servers[i]);
+    //gives us root if we don't already have it
+    if (!hasRootAccess(servers[i]) && getServerNumPortsRequired(servers[i])<attackPossible)
+    {
+    exec("attack.script","home",1, servers[i]);
     }
-    i++;
+    
+    //starts copycat on the next node
+    exec("copycat.script",servers[i]);
+    }
 }
-//checks how much ram it can use, kills the script and then runs as many genhacks as it can
-avalibleRam=getServerRam(host)[0];
-scriptSize=getScriptRam("genhack.script",host);
-spawn("genhack.script", Math.floor(avalibleRam/scriptSize), host);
+sleep(10000);
+if (!(forbiddenServers.includes(host))){spawn("getName.script",1,host)}
