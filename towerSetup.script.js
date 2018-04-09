08440af58d0e6136f426f30fecd27563c0bf4478
Sapsap11 <38094709+Sapsap11@users.noreply.github.com>
2018-04-09 11:29:00 +0200

    automatic
    now gets your server list automatically, requires serverScan and validateServerList to work

--- towerSetup.script.js
+++ towerSetup.script.js
@@ -1,13 +1,12 @@
-towers=["MegaBoi","GigaBoi","UltraBoi","Doombot","Sauron","The-eye","Origin","Singularity"];
+towers=getPurchasedServers();
 host=getHostname();
+scripts=["grower.script","weaker.script","hacker.script"];
 for (i=0;i<towers.length;i++)
 {
-scp("grower.script",towers[i]);
-scp("weaker.script",towers[i]);
-scp("hacker.script",towers[i]);
-if (!isRunning("watchTower.script",host,towers[i]))
-{
-    run("watchTower.script",1,towers[i]);
-    tprint(towers[i]+" is now online");
-}
+    scp(scripts, "home", towers[i]);
+    if (!isRunning("watchTower.script",host,towers[i]))
+    {
+        run("watchTower.script",1,towers[i]);
+        tprint(towers[i]+" is now online");
+    }
 }
