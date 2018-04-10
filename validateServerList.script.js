ec83c0c9af9f8a6afbec782d57e7822067923c84
Sapsap11 <38094709+Sapsap11@users.noreply.github.com>
2018-04-10 09:59:06 +0200

    restraint
    it no longer tries to hack servers it doesn't have root access for

--- validateServerList.script.js
+++ validateServerList.script.js
@@ -1,7 +1,7 @@
 serverList=read("altScan").split(",");
 validated=[];
 for (i=0;i<serverList.length;i++){
-    if (getHackingLevel()>=getServerRequiredHackingLevel(serverList[i]) && getServerMaxMoney(serverList[i])!==0)
+    if (hasRootAccess(serverList[i]) && getHackingLevel()>=getServerRequiredHackingLevel(serverList[i]) && getServerMaxMoney(serverList[i])!==0)
     {
         validated.push(serverList[i]);
     }
